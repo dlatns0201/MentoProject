21bca1ea2e6493443fe2afe27bc9082a028dabd4
Taehyun Lim <dlatns0201@naver.com>
2018-08-27 21:05:44 +0900

    됫으면 좋겟다

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var express=require('express');
-var mongoose=require('mongoose');
-var socketio=require('socket.io');
-var app=express();
+var express = require('express');
+var mongoose = require('mongoose');
+var socketio = require('socket.io');
+var app = express();
 var ChatSchema = mongoose.Schema({
     room_number: { type: String, required: true, unique: true },
     sender_id: [{ type: String }],
@@ -11,9 +11,9 @@ var ChatSchema = mongoose.Schema({
     date: [{ type: Date, "default": Date.now() }]
 });
 var chat = mongoose.model('chat', ChatSchema);
-var Dburl="mongodb://localhost:27017/local";
+var Dburl = "mongodb://localhost:27017/local";
 mongoose.connect(Dburl);
-var database=mongoose.connection;
+var database = mongoose.connection;
 
 var connect = {};
 var server = http.createServer(app).listen(3000, function () {
@@ -33,18 +33,18 @@ io.sockets.on('connection', function (socket) {
                 var object;
                 for (var i = 0; i < result[0].message.length; i++) {
                     if (connect[result[0].sender_id[i]] == socket.id) {
-                        object[i] = {"name":"나","message":result[0].message[i]};
+                        object[i] = { "name": "나", "message": result[0].message[i] };
                     }
-                    else{
-                        object[i]={"name":data.listener_id,"message":result[0].message[i]};
+                    else {
+                        object[i] = { "name": data.listener_id, "message": result[0].message[i] };
                     }
                 }
-                socket.emit('loadChat',object);
+                socket.emit('loadChat', object);
             }
         });
     });
     socket.on('message', function (data) {
-        chat.find().or([{ "room_number": data.sender_id + ":" + data.listener_id }, { "room_number": data.listener_id + ":" + data.sender_id }], function (err, result) {
+        chat.find(function (err, result) {
             if (result.length > 0) {
                 var sender = result[0].sender_id;
                 sender[sender.length] = data.sender_id;
@@ -53,11 +53,12 @@ io.sockets.on('connection', function (socket) {
                 var message = result[0].message;
                 message[message.length] = data.message;
                 console.log(result[0].room_number);
+                console.log(result);
                 chat.where({ "room_number": result[0].room_number }).update({ "sender_id": sender, "listener_id": listener, "message": message }, function () { });
             } else {
                 console.log("방이 없음");
             }
-        });
+        }).or([{ "room_number": data.sender_id + ":" + data.listener_id }, { "room_number": data.listener_id + ":" + data.sender_id }]);
         io.to(connect[data.sender_id]).emit('message', { name: "나", message: data.message });
         io.to(connect[data.listener_id]).emit('message', { name: data.sender_id, message: data.message });
     });
